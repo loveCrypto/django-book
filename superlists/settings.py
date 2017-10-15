e3611006dce82b5b960162ce5cc4cf7da35ddce2
hobbyhorse <hobbyhorse@riseup.net>
2017-10-15 18:40:10 +0200

    Add gunicorn to virualenv requirements

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'xz$%%=2-3nuvsupd6%m0bku2m_-xrpum^*ek*vmsdkz)eixwvd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+        'django.illu.arcturus.uberspace.de',
+        ]
 
 
 # Application definition
@@ -104,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -119,4 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '/var/www/virtual/illu/django.illu.arcturus.uberspace.de/static'))
+
+# Gunicorn
+USE_X_FORWARDED_HOST = True
