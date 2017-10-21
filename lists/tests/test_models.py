ca5cfdc04dcd7fe8250206cd3c06ffb533d51139
illu <illu@localhost.localdomain>
2017-10-21 16:27:37 +0200

    Adjust new list view to do model validation

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
+
 
 class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
         list_ = List()
         list_.save()
-        
         text_one = 'The first (ever) list item'
         text_two = 'Item the second'
 
@@ -14,7 +15,6 @@ class ListAndItemModelsTest(TestCase):
         first_item.text = text_one
         first_item.list = list_
         first_item.save()
-        
         second_item = Item()
         second_item.text = text_two
         second_item.list = list_
@@ -33,3 +33,9 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(second_saved_item.text, text_two)
         self.assertEqual(second_saved_item.list, list_)
 
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,33 +1,48 @@
 from django.test import TestCase
 from lists.models import Item, List
+from django.utils.html import escape
 
 # Don't test constants
 # Don't repeat yourself
+
 
 class HomePageTest(TestCase):
 
     def get_text(self):
         text = 'A new list item'
         return text
-    
+
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
 
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        
+
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-    
     def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new',
+                                    data={'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class NewItemTest(TestCase):
@@ -44,7 +59,7 @@ class NewItemTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
-    
+
     def test_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -55,6 +70,7 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
 
 class ListViewTest(TestCase):
 
@@ -67,11 +83,10 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
         Item.objects.create(text='itemey 1', list=correct_list)
         Item.objects.create(text='itemey 2', list=correct_list)
-        
+
         other_list = List.objects.create()
         Item.objects.create(text='other list item 1', list=other_list)
         Item.objects.create(text='other list item 2', list=other_list)
-        
 
         response = self.client.get(f'/lists/{correct_list.id}/')
 
@@ -79,10 +94,9 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
-    
+
     def test_passes_correct_list_to_template(self):
         correct_list = List.objects.create()
         other_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
-
